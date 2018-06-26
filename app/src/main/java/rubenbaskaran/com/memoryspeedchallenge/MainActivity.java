afcfad1275b8d86b5ed4709ef10e491800bbf97b
Ruben Baskaran <rbaskaran92@gmail.com>
2018-06-26 19:44:34 +0200

    Implemented 5x5 button grid with tags

--- app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/MainActivity.java
+++ app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/MainActivity.java
@@ -2,14 +2,22 @@ package rubenbaskaran.com.memoryspeedchallenge;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view)
+    {
+        Button btn = (Button) view;
+        Log.e("Btn clicked", btn.getTag().toString());
     }
 }
