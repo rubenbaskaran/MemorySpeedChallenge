578502449630e3f926bb426816626a6bc84407ac
Ruben Baskaran <rbaskaran92@gmail.com>
2018-06-26 22:56:09 +0200

    Implemented animation in asynctask

--- app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/MainActivity.java
+++ app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/MainActivity.java
@@ -1,23 +1,71 @@
 package rubenbaskaran.com.memoryspeedchallenge;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity
 {
+    View root;
+    int[] testRoute = {7, 9, 13, 17, 19};
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        root = LayoutInflater.from(this).inflate(R.layout.activity_main, null);
+        setContentView(root);
     }
 
     public void onClick(View view)
     {
-        Button btn = (Button) view;
-        Log.e("Btn clicked", btn.getTag().toString());
+        AsyncCreateRoute asyncCreateRoute = new AsyncCreateRoute();
+        asyncCreateRoute.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private class AsyncCreateRoute extends AsyncTask
+    {
+        @Override
+        protected Object doInBackground(Object[] objects)
+        {
+            for (int item : testRoute)
+            {
+                Object[] output = {true, item};
+                publishProgress(output);
+                try
+                {
+                    Thread.sleep(200);
+                }
+                catch (InterruptedException e)
+                {
+                    Log.e("Error", "Error in AsyncCreateRoute");
+                }
+                output[0] = false;
+                publishProgress(output);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Object[] values)
+        {
+            super.onProgressUpdate(values);
+            Button btn = root.findViewWithTag(String.valueOf(values[1]));
+
+            if (((boolean) values[0]))
+            {
+                btn.setBackgroundColor(Color.BLACK);
+            }
+            else
+            {
+                btn.setBackgroundColor(Color.WHITE);
+            }
+        }
     }
 }
