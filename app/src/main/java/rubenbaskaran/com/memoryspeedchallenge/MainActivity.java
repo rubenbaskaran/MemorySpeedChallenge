ceb7ecbb6091256d1dbd7e69d2441453f90f0c35
Ruben Baskaran <rbaskaran92@gmail.com>
2018-06-27 14:35:46 +0200

    Implemented user input handling

--- app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/MainActivity.java
+++ app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,12 +25,34 @@ public class MainActivity extends Activity
         root = LayoutInflater.from(this).inflate(R.layout.activity_main, null);
         setContentView(root);
         startGameBtn = findViewById(R.id.startGameBtn);
+        EnableGridButtons(false);
     }
 
     public void onClick(View view)
     {
         AsyncCreateRoute asyncCreateRoute = new AsyncCreateRoute();
         asyncCreateRoute.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    public void onGridButtonClick(View view)
+    {
+        if (Integer.parseInt(view.getTag().toString()) == route.get(0))
+        {
+            route.remove(0);
+            // TODO: Set color
+            if (route.isEmpty())
+            {
+                Toast.makeText(this, "Congratulations! Level completed!", Toast.LENGTH_LONG).show();
+                EnableGridButtons(false);
+                startGameBtn.setEnabled(true);
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Sorry! You failed!", Toast.LENGTH_LONG).show();
+            EnableGridButtons(false);
+            startGameBtn.setEnabled(true);
+        }
     }
 
     private class AsyncCreateRoute extends AsyncTask
@@ -41,6 +64,7 @@ public class MainActivity extends Activity
             GameAlgorithm gameAlgorithm = new GameAlgorithm();
             route = gameAlgorithm.GenerateRoute(10);
             startGameBtn.setEnabled(false);
+            EnableGridButtons(false);
         }
 
         @Override
@@ -85,7 +109,36 @@ public class MainActivity extends Activity
         protected void onPostExecute(Object o)
         {
             super.onPostExecute(o);
-            startGameBtn.setEnabled(true);
+            EnableGridButtons(true);
         }
+    }
+
+    private void EnableGridButtons(boolean input)
+    {
+        root.findViewWithTag("1").setEnabled(input);
+        root.findViewWithTag("2").setEnabled(input);
+        root.findViewWithTag("3").setEnabled(input);
+        root.findViewWithTag("4").setEnabled(input);
+        root.findViewWithTag("5").setEnabled(input);
+        root.findViewWithTag("6").setEnabled(input);
+        root.findViewWithTag("7").setEnabled(input);
+        root.findViewWithTag("8").setEnabled(input);
+        root.findViewWithTag("9").setEnabled(input);
+        root.findViewWithTag("10").setEnabled(input);
+        root.findViewWithTag("11").setEnabled(input);
+        root.findViewWithTag("12").setEnabled(input);
+        root.findViewWithTag("13").setEnabled(input);
+        root.findViewWithTag("14").setEnabled(input);
+        root.findViewWithTag("15").setEnabled(input);
+        root.findViewWithTag("16").setEnabled(input);
+        root.findViewWithTag("17").setEnabled(input);
+        root.findViewWithTag("18").setEnabled(input);
+        root.findViewWithTag("19").setEnabled(input);
+        root.findViewWithTag("20").setEnabled(input);
+        root.findViewWithTag("21").setEnabled(input);
+        root.findViewWithTag("22").setEnabled(input);
+        root.findViewWithTag("23").setEnabled(input);
+        root.findViewWithTag("24").setEnabled(input);
+        root.findViewWithTag("25").setEnabled(input);
     }
 }
