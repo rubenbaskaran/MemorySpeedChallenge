036635be94abe23b080c97f6c58278069a406310
Ruben Baskaran <rbaskaran92@gmail.com>
2018-07-01 15:53:35 +0200

    Added highscore activity and layout

---
+++ app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/Activities/HighscoreActivity.java
@@ -0,0 +1,17 @@
+package rubenbaskaran.com.memoryspeedchallenge.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import rubenbaskaran.com.memoryspeedchallenge.R;
+
+public class HighscoreActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
