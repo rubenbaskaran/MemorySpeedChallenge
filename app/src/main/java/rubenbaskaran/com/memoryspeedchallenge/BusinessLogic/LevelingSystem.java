65ba6d5a431fbbc95c3f2c2bc0635f78a24bd1af
Ruben Baskaran <rbaskaran92@gmail.com>
2018-07-02 18:33:49 +0200

    Reduced minimum scores

--- app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/BusinessLogic/LevelingSystem.java
+++ app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/BusinessLogic/LevelingSystem.java
@@ -125,53 +125,43 @@ public class LevelingSystem
 
     public static int GetMinimumScore(int currentLevel)
     {
-        return 500;
-
-//        switch (currentLevel)
-//        {
-//            case 0:
-//                return 100;
-//            case 1:
-//                return 200;
-//            case 2:
-//                return 300;
-//            case 3:
-//                return 400;
-//            case 4:
-//                return 500;
-//            case 5:
-//            case 6:
-//            case 7:
-//            case 8:
-//            case 9:
-//            case 10:
-//                return 600;
-//            case 11:
-//                return 700;
-//            case 12:
-//                return 800;
-//            case 13:
-//                return 900;
-//            case 14:
-//                return 1000;
-//            case 15:
-//                return 1100;
-//            case 16:
-//            case 17:
-//            case 18:
-//            case 19:
-//            case 20:
-//                return 1500;
-//            case 21:
-//            case 22:
-//            case 23:
-//            case 24:
-//                return 2000;
-//            case 25:
-//            case 26:
-//                return 500;
-//            default:
-//                return 1;
-//        }
+        switch (currentLevel)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                return 100;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                return 200;
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+                return 300;
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+                return 400;
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+                return 500;
+            case 25:
+            case 26:
+                return 600;
+            default:
+                return 1;
+        }
     }
 }
