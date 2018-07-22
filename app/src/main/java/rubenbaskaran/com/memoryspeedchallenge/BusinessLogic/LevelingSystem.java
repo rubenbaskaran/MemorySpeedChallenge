19ebcb8c5be679e3ec11e111d54e1f18ceb28cf0
Ruben Baskaran <rbaskaran92@gmail.com>
2018-07-22 14:02:31 +0200

    Increased speed for each level

--- app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/BusinessLogic/LevelingSystem.java
+++ app/src/main/java/rubenbaskaran/com/memoryspeedchallenge/BusinessLogic/LevelingSystem.java
@@ -83,43 +83,55 @@ public class LevelingSystem
         switch (currentLevel)
         {
             case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
                 return 500;
-            case 11:
-                return 450;
-            case 12:
+            case 1:
+                return 480;
+            case 2:
+                return 460;
+            case 3:
+                return 440;
+            case 4:
+                return 420;
+            case 5:
                 return 400;
-            case 13:
-                return 350;
-            case 14:
+            case 6:
+                return 380;
+            case 7:
+                return 360;
+            case 8:
+                return 340;
+            case 9:
+                return 320;
+            case 10:
                 return 300;
+            case 11:
+                return 280;
+            case 12:
+                return 260;
+            case 13:
+                return 240;
+            case 14:
+                return 220;
             case 15:
-                return 250;
-            case 16:
-            case 17:
-            case 18:
-            case 19:
-            case 20:
                 return 200;
+            case 16:
+                return 180;
+            case 17:
+                return 160;
+            case 18:
+                return 140;
+            case 19:
+                return 120;
+            case 20:
             case 21:
             case 22:
             case 23:
             case 24:
-                return 150;
             case 25:
             case 26:
                 return 100;
             default:
-                return 2000;
+                return 1000;
         }
     }
 
@@ -161,7 +173,7 @@ public class LevelingSystem
             case 26:
                 return 600;
             default:
-                return 1;
+                return 1000;
         }
     }
 }
